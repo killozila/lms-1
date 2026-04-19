23c1556b9b50f553340c6f3daf6add156ee2d684
Alianderson Dev <99621023+killozila@users.noreply.github.com>
2026-04-19 09:05:50 -0300

    att1.01

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,11 @@
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
-import { TutorWidget } from "@/components/tutor";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <div>{children}</div>
       <SanityLive />
-      <TutorWidget />
     </ClerkProvider>
   );
 }
--- components/tutor/index.ts
+++
@@ -1,4 +0,0 @@
-export { TutorWidget } from "./TutorWidget";
-export { TutorChat } from "./TutorChat";
-export { TutorMessages } from "./TutorMessages";
-export { TutorProvider, useTutor } from "./TutorContext";
